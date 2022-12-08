efd4e58650c4e84dbaa8b731bf8ab5b61f3f72d2
nytrea <anoushsan@gmail.com>
2022-12-08 16:47:01 -0500

    Update product-details

--- src/routes/ProductDetails/product-details.jsx
+++ src/routes/ProductDetails/product-details.jsx
@@ -10,10 +10,10 @@ export default function ProductPage(){
             Price 
             Description
             Add to Cart Button */}
-            <img src={item.image} className="product-image"></img>
+            {/* <img src={item.image} className="product-image"></img>
             <h3 className="product-title">{item.title}</h3>
             <h3 className="product-price">{format_price(item.price)}</h3>
-            <h4 className="product-description">{item.description}</h4>
+            <h4 className="product-description">{item.description}</h4> */}
         </div>
     );
 }
